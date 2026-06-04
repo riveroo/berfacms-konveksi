9392173b16fb8461921b22481a82f298aa39b340
emailnyaberly <emailnyaberly@gmail.com>
2026-06-04 11:01:45 +0700

    add edit & hapus opening balance

--- app/Filament/Resources/OpeningBalanceResource.php
+++ app/Filament/Resources/OpeningBalanceResource.php
@@ -98,7 +98,8 @@ class OpeningBalanceResource extends Resource
                     }),
             ])
             ->actions([
-                //
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
